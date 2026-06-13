00e50a7d1370fd11bbe4183fc56ac6491972b333
Dhruv Jain <dhruvjain0412@gmail.com>
2026-06-14 02:52:29 +0530

    Fix bg changer GitHub Pages path

--- 04bgChanger/vite.config.js
+++ 04bgChanger/vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/chai-aur-react/04bgchanger/",
+  base: "/chai-aur-react/04bgChanger/",
 });
